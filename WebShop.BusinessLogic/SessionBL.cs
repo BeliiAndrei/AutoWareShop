489bfa2bb24d1a9893cef061979aa437b02b0305
andrei.belii.04@gmail.com <andrei.belii.04@gmail.com>
2025-03-10 23:20:39 +0200

    BL projects added to solution

---
+++ WebShop.BusinessLogic/SessionBL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebShop.BusinessLogic.Core;
+using WebShop.BusinessLogic.Interfaces;
+
+namespace WebShop.BusinessLogic
+{
+    public class SessionBL : UserApi, ISession
+    {
+
+    }
+}
