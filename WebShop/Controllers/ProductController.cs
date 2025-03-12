67d50e85eff498eae811ace96727f15aa37bd4cc
andrei.belii.04@gmail.com <andrei.belii.04@gmail.com>
2025-03-12 16:29:03 +0200

    Minor front changes to some pages

--- WebShop/Controllers/ProductController.cs
+++ WebShop/Controllers/ProductController.cs
@@ -20,6 +20,16 @@ namespace WebShop.Controllers
             item.BrandName = "Filtron";
             item.Article = "OP525";
             item.Code = 465907;
+            item.Images = new List<string>();
+            item.Images.Add("1");
+            item.Images.Add("7");
+            item.Images.Add("6");
+            item.Images.Add("4");
+            item.Images.Add("5");
+            item.Images.Add("2");
+            item.Images.Add("3");
+            item.Description = "Ма́сляный фи́льтр — устройство, предназначенное для удаления загрязнений " +
+                "из моторных, компрессорных, турбинных, трансмиссионных, смазочных масел, гидравлических жидкостей (жидкость для автоматической коробки перемены передач, жидкость для гидравлического усилителя рулевого управления) и др.";
             return View(item);
         }
     }
