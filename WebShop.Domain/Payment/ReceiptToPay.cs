d5f2305e766f874d6d3008e64e3bd03bc801c394
dmitrii.anghis1@isa.utm.md <dmitrii.anghis1@isa.utm.md>
2025-03-16 21:49:46 +0200

    BuisnessLogic Pay.. Pro.. Use..

---
+++ WebShop.Domain/Payment/ReceiptToPay.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebShop.Domain.Product
+{
+    public class ReceiptToPay
+    {
+        public string Id { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        public List<ProductDTO> Products { get; set; }
+    }
+}
