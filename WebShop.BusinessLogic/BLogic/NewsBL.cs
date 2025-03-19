ce7dee68ca6f2eab5508e361b5447825d30346ff
dmitrii.anghis1@isa.utm.md <dmitrii.anghis1@isa.utm.md>
2025-03-19 14:57:17 +0200

    BL_News

---
+++ WebShop.BusinessLogic/BLogic/NewsBL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebShop.BusinessLogic.Core;
+using WebShop.BusinessLogic.Interfaces;
+
+namespace WebShop.BusinessLogic.BLogic
+{
+    public class NewsBL : AdminApi, INews
+    {
+        public void CreateNews(News news)
+        {
+        }
+
+        public void UpdateNews(News news)
+        {
+        }
+
+        public void DeleteNews(int newsId)
+        {
+        }
+    }
+}
