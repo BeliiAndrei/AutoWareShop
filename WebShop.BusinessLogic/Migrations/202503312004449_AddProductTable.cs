0cd1e8ce951facc9c13dff4bf069d7fd3ec5783d
andrei.belii.04@gmail.com <andrei.belii.04@gmail.com>
2025-04-01 08:10:26 +0300

    If user is banned, a message is displayed in header

---
+++ WebShop.BusinessLogic/Migrations/202503312004449_AddProductTable.cs
@@ -0,0 +1,16 @@
+﻿namespace WebShop.BusinessLogic.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddProductTable : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- WebShop.Domain/Product/ProductDBTable.cs
+++ WebShop.Domain/Product/ProductDBTable.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using WebShop.Domain.Enumerables;
 
 namespace WebShop.Domain.Product
 {
@@ -37,6 +34,12 @@ namespace WebShop.Domain.Product
 
         [Display(Name = "Description")]
         public string Description { get; set; }
+
+        [Display(Name = "Status")]
+        public ProductStatus Status { get; set; }
+
+        [Display(Name = "Category")]
+        public string Category { get; set; }
 
     }
 }
