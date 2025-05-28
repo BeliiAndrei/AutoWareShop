0c10d738c8375d383126234e402ec1c14269e010
dmitrii.anghis1@isa.utm.md <dmitrii.anghis1@isa.utm.md>
2025-05-29 01:23:45 +0300

    7

--- WebShop/Controllers/HomeController.cs
+++ WebShop/Controllers/HomeController.cs
@@ -1,60 +1,78 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using WebShop.BusinessLogic.Interfaces;
-using WebShop.Domain.Product;
-using WebShop.Models;
-using WebShop.Models.MainPageModels;
+﻿    using System.Collections.Generic;
+    using System.Web.Mvc;
+    using WebShop.BusinessLogic.Interfaces;
+    using WebShop.Domain.News;
+    using WebShop.Domain.Product;
+    using WebShop.Models;
+    using WebShop.Models.MainPageModels;
 
-namespace WebShop.Controllers
-{
-    public class HomeController : BaseController
+    namespace WebShop.Controllers
     {
-        IProduct _product;
-        //INews _news;
-        public HomeController()
+        public class HomeController : BaseController
         {
-            var bl = new BusinessLogic.BusinessLogic();
-            _product = bl.GetProductBl();
-            //_news = bl.GetNewsBl();
-        }
-        public ActionResult Index()
-        {
-            //Не используется, поэтому Redirect
-            //return View();
-            return RedirectPermanent("MainPage");
-        }
+            private readonly IProduct _product;
+            private readonly INews _news;
 
-        public ActionResult MainPage()
-        {
-            MainPageModel model = new MainPageModel();
-            //model.News = _news.GetAllNews();
-            // TO DO. //
-            // Добавить передачу в модель списка новостей для отображения на главной странице
-
-            var bonusProducts = _product.GetProductsByStatus("bonus", 1, 8, 0m, int.MaxValue, true, null);
-            var productsForView = new List<ProductCardViewModel>();
-            foreach (ProductDTO p in bonusProducts.products)
+            public HomeController()
             {
-                var product = new ProductCardViewModel
-                {
-                    ProductName = p.Name,
-                    BrandName = p.Producer,
-                    Code = p.Id,
-                    Article = p.Article,
-                    Description = p.Description,
-                    Image = p.ImageNumber,
-                    Price = p.Price,
-                    Quantity = p.Quantity,
-                    Status = p.Status.ToString()
-                };
-                productsForView.Add(product);
+                var bl = new BusinessLogic.BusinessLogic();
+                _product = bl.GetProductBl();
+                _news = bl.GetNewsBl();
             }
-            
-            model.SearchResults = productsForView;
-            return View(model);
+
+            public ActionResult Index()
+            {
+                return RedirectPermanent("MainPage");
+            }
+
+            public ActionResult MainPage()
+            {
+                var model = new MainPageModel
+                {
+                    // Достаем все новости подряд, без сортировки и ограничений
+                    News = _news.GetAllNews(),
+
+                    // Достаем товары (как было в оригинале)
+                    SearchResults = GetBonusProducts()
+                };
+
+                return View(model);
+            }
+
+            private List<ProductCardViewModel> GetBonusProducts()
+            {
+                var bonusProducts = _product.GetProductsByStatus("bonus", 1, 8, 0m, int.MaxValue, true, null);
+                var productsForView = new List<ProductCardViewModel>();
+
+                foreach (var p in bonusProducts.products)
+                {
+                    productsForView.Add(new ProductCardViewModel
+                    {
+                        ProductName = p.Name,
+                        BrandName = p.Producer,
+                        Code = p.Id,
+                        Article = p.Article,
+                        Description = p.Description,
+                        Image = p.ImageNumber,
+                        Price = p.Price,
+                        Quantity = p.Quantity,
+                        Status = p.Status.ToString()
+                    });
+                }
+
+                return productsForView;
+            }
+            public FileContentResult GetNewsImage(int newsId)
+            {
+                var news = _news.GetNewsByIdAction(newsId);
+                if (news?.ImageData != null)
+                {
+                    return File(news.ImageData, news.ImageMimeType ?? "image/jpeg");
+                }
+                // Заглушка, если нет изображения
+                var defaultImagePath = Server.MapPath("~/assets/images/content/news-default.jpg");
+                var imageBytes = System.IO.File.ReadAllBytes(defaultImagePath);
+                return File(imageBytes, "image/jpeg");
+            }
         }
     }
-}
--- WebShop/Models/MainPageModels/MainPageModel.cs
+++ WebShop/Models/MainPageModels/MainPageModel.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.Generic;
+using WebShop.Domain.News;
+using WebShop.Models;
 
 namespace WebShop.Models.MainPageModels
 {
     public class MainPageModel
     {
-        public List<NewsViewModel> News { get; set; }
         public List<ProductCardViewModel> SearchResults { get; set; }
+        public List<News> News { get; set; } // Просто список новостей, без лишних условий
     }
 }
